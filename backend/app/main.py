e463e8ba9e5bd193adf9802a96c1360f8d52946b
theKaido <72819473+theKaido@users.noreply.github.com>
2026-07-02 19:49:01 +0200

    Add restaurants router and auth router

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.routers.allergene import router as router_allergene
 from app.routers.ingredient import router as router_ingredient
 from app.routers.plat import router as router_plat
+from app.routers.restaurant import router as router_restaurant
+from app.routers.auth import router as router_auth
 
 
 app = FastAPI()
@@ -9,8 +11,9 @@ app = FastAPI()
 app.include_router(router_allergene, prefix="/allergenes", tags=["Allergenes"])
 app.include_router(router_ingredient, prefix="/ingredients", tags=["Ingredients"])
 app.include_router(router_plat, prefix="/plats", tags=["Plats"])
-
+app.include_router(router_restaurant, prefix="/restaurants", tags=["Restaurants"])
+app.include_router(router_auth, prefix="/auth", tags=["Authentificiation"])
 
 @app.get("/health")
 def health():
-    return {"status": "ok"}
+    return {"status": "ok"} 
